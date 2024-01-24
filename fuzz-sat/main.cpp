0015dc4fb52d6cf9e3778fa5e174892f97dd3d91
stytSofo <ssofok02@ucy.ac.cy>
2024-01-24 15:02:49 +0000

    Process tmeout and error log

--- fuzz-sat/main.cpp
+++ fuzz-sat/main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <future>
 #include "subprocess.hpp"
-
 
 int main(int argc, char *argv[])
 {
     std::string SATPath = argv[1];
     std::string InputPath = argv[2];
 
+    auto SUTProcess = subprocess::Popen({SATPath, InputPath}, subprocess::output(subprocess::PIPE), subprocess::error(subprocess::PIPE));
 
-    auto SUTProcess = subprocess::Popen({SATPath, InputPath}, subprocess::output(subprocess::PIPE), subprocess::error(subprocess::PIPE) );
+    std::future<std::pair<subprocess::Buffer,subprocess::Buffer>> future = std::async(std::launch::async, [&SUTProcess]{
+        return SUTProcess.communicate();
+    });
 
-    auto [output,error_output] = SUTProcess.communicate();
-    
-    std::cerr << "Error: " << error_output.buf.data();
-    
+    if (future.wait_for(std::chrono::seconds(5)) == std::future_status::timeout)
+    {
+        std::cerr << "SAT killed timeout reached -> ERROR: Infinite LOOP\n";
+        SUTProcess.kill(15);
+    }
+    else
+    {
+        auto [output, error_output] = future.get();
+        std::cout << output.buf.data();
+        std::cerr << "Error: " << error_output.buf.data();
+    }
 }
