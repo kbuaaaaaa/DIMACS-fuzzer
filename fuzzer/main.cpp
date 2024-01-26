9641b00a8953580a8923826217dcd70996810e8a
stytSofo <ssofok02@ucy.ac.cy>
2024-01-26 11:59:10 +0000

    Global counter ouput file

--- fuzzer/main.cpp
+++ fuzzer/main.cpp
@@ -1,18 +1,28 @@
-
 #include "main.h"
+
+int COUNTER = 0;
 
 int main(int argc, char *argv[])
 {
     std::string SATPath = argv[1];
     std::string InputPath = argv[2];
 
-    auto SUTProcess = subprocess::Popen({SATPath, InputPath}, subprocess::output(subprocess::PIPE), subprocess::error(subprocess::PIPE));
+    // seed
+    while (true)
+    {
+        // generate input
 
-    execute(SUTProcess);
+        auto SUTProcess = subprocess::Popen({SATPath, InputPath}, subprocess::output(subprocess::PIPE), subprocess::error(subprocess::PIPE));
+
+        execute(SUTProcess);
+        
+    }
+    
 }
 
 void save_to_file(char *output, int i)
 {
+    //ASYNC WRITE HERE
     std::string name = "fuzzed-tests/test" + std::to_string(i) + ".txt";
     std::ofstream file(name);
 
@@ -29,14 +39,17 @@ void execute(subprocess::Popen &SUTProcess)
     {
         std::cerr << "SAT killed timeout reached -> ERROR: Infinite LOOP\n";
         SUTProcess.kill(15);
+
+        save_to_file("SAT killed timeout reached -> ERROR: Infinite LOOP\n", COUNTER);
+        COUNTER = COUNTER + 1;
     }
     else if (SUTProcess.retcode() != 0)
     {
         auto output = SUTProcess.communicate();
         std::cout << output.first.buf.data();
-        
-        save_to_file(output.second.buf.data(), 1);
-        std::cerr << "Error: " << output.second.buf.data();
+
+        save_to_file(output.second.buf.data(), COUNTER);
+        COUNTER = COUNTER + 1;
     }
 }
 
