a11e1d0916119e0e9f3949229beea359bce728bf
kbuaaaaaa <karnbongkot@gmail.com>
2024-01-29 18:48:51 +0000

    Introduce file saving thread (+ concurrency problem)

--- fuzzer/main.cpp
+++ fuzzer/main.cpp
@@ -7,25 +7,11 @@ int FilesCopied = 0;
 
 std::mutex InputCounterMutex;
 std::mutex CurrentCounterMutex;
-std::mutex FilesCopiedMutex;
-std::mutex ErrorsMutex;
+
+ThreadSafeQueue<SaveToFileParams> saveToFileQueue;
 
 int main(int argc, char *argv[])
 {
-    // try
-    // {
-    //     // you can pass http::InternetProtocol::V6 to Request to make an IPv6 request
-    //     http::Request request{"http://172.167.164.98/"};
-
-    //     // send a get request
-    //     const auto response = request.send("GET");
-    //     std::cout << std::string{response.body.begin(), response.body.end()} << '\n'; // print the result
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cerr << "Request failed, error: " << e.what() << '\n';
-    // }
-
     std::string SATPath = argv[1];
     int seed = atoi(argv[2]);
     srand(seed);
@@ -35,13 +21,23 @@ int main(int argc, char *argv[])
 
     // generate input
     std::thread InputGenerationThread(generate_cnf_files);
+    std::thread OutputProcessingThread(process_output);
     std::thread FuzzingThread1(fuzz, SATPath);
     std::thread FuzzingThread2(fuzz, SATPath);
     fuzz(SATPath);
 
     FuzzingThread1.join();
     FuzzingThread2.join();
+    OutputProcessingThread.join();
     InputGenerationThread.join();
+}
+
+void process_output()
+{
+    while(true){
+        SaveToFileParams params = saveToFileQueue.pop();
+        save_to_file(params.raw_error_output, params.CurrentInput);
+    }
 }
 
 void fuzz(std::string SATPath){
@@ -69,20 +65,37 @@ void fuzz(std::string SATPath){
 
 void generate_cnf_files()
 {
+    int InputChoice;
     while(true){
-        if (rand() % 101 < 30)
-            generate_correct_cnf_files();
+        InputChoice = rand() % 101;
+        if (InputChoice < 10)
+            generate_complex_correct_cnf_files();
+        else if (InputChoice < 30)
+            generate_simple_correct_cnf_files();
         else
             generate_trash_cnf_files();
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
 
-void generate_correct_cnf_files()
+void generate_complex_correct_cnf_files()
 {
     std::string name = "inputs/AUTOGEN_" + std::to_string(INPUT_COUNTER) + ".cnf";
     std::ofstream file(name);
 
-    file << generate_correct_cnf() << "\n";
+    file << generate_complex_correct_cnf() << "\n";
+    file.close();
+    InputCounterMutex.lock();
+    INPUT_COUNTER++;
+    InputCounterMutex.unlock();
+}
+
+void generate_simple_correct_cnf_files()
+{
+    std::string name = "inputs/AUTOGEN_" + std::to_string(INPUT_COUNTER) + ".cnf";
+    std::ofstream file(name);
+
+    file << generate_simple_correct_cnf() << "\n";
     file.close();
     InputCounterMutex.lock();
     INPUT_COUNTER++;
@@ -144,11 +157,11 @@ int pick(int from, int to) {
     return (std::rand() % (to - from + 1)) + from;
 }
 
-std::string generate_correct_cnf()
+std::string generate_complex_correct_cnf()
 {
     std::stringstream output;
-    int max_width = pick(10, 100);
-    int nlayers = pick(1, 100);
+    int max_width = pick(10, 70);
+    int nlayers = pick(1, 20);
     std::vector<std::vector<int>> unused(nlayers);
     std::vector<int> width(nlayers), low(nlayers), high(nlayers), clauses(nlayers), nunused(nlayers);
 
@@ -396,8 +409,6 @@ void save_to_file(const char *raw_error_output, long CurrentInput)
     std::string name = "fuzzed-tests/test_error_" + std::to_string(CurrentInput) + ".txt";
     std::string grep_content = "";
     std::ofstream error_file(name);
-    FilesCopiedMutex.lock();
-    ErrorsMutex.lock();
     for (size_t j = 0; j < REGEX_ERRORS; j++)
     {
 
@@ -485,8 +496,6 @@ void save_to_file(const char *raw_error_output, long CurrentInput)
     printf("----------------------------------------------------\n");
 
     error_file.close();
-    ErrorsMutex.unlock();
-    FilesCopiedMutex.unlock();
 }
 
 void execute(subprocess::Popen &SUTProcess, long CurrentInput)
@@ -498,12 +507,12 @@ void execute(subprocess::Popen &SUTProcess, long CurrentInput)
     {
         std::cerr << "SAT killed timeout reached -> ERROR: Infinite LOOP\n";
         SUTProcess.kill(15);
-        save_to_file("SAT killed timeout reached -> ERROR: Infinite LOOP\n", CurrentInput);
+        saveToFileQueue.push({"SAT killed timeout reached -> ERROR: Infinite LOOP\n", CurrentInput});
     }
     else if (SUTProcess.retcode() != 0)
     {
         auto output = SUTProcess.communicate();
-        save_to_file(output.second.buf.data(), CurrentInput);
+        saveToFileQueue.push({output.second.buf.data(), CurrentInput});
     }
 }
 
@@ -576,9 +585,4 @@ void run_one_time_edge_cases(std::string SATPath)
     }
     CURRENT_COUNTER = INPUT_COUNTER;
 
-    // std::string InputPath = "inputs/WRONG_NAME.cnf";
-    // auto SUTProcess = subprocess::Popen({SATPath, InputPath}, subprocess::output(subprocess::PIPE), subprocess::error(subprocess::PIPE));
-    // execute(SUTProcess);
-    // INPUT_COUNTER += 1;
-    // CURRENT_COUNTER = INPUT_COUNTER;
 }
