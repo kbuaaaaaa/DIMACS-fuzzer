d09b77468fb9eb4ba7daaf843cb3340f048e8fd1
Sergio Miguez <sm3823@ic.ac.uk>
2024-02-01 20:32:57 +0000

    only existing folders

--- build.sh
+++ build.sh
@@ -2,21 +2,20 @@
 set -x # Display every executed instruction
 set -e # Fail script as soon as instruction fails
 
-if [ -d ./fuzzed-tests ]; then
-    echo "Directory exists"
-    rm -rf ./fuzzed-tests
-fi
+# if [ -d ./fuzzed-tests ]; then
+#     echo "Directory exists"
+#     rm -rf ./fuzzed-tests
+# fi
 
-rm -rf inputs/AUTOGEN*
-mkdir ./fuzzed-tests
-chmod 777 ./fuzzed-tests/
-chmod +x solvers/solver1/runsat.sh
-chmod +x solvers/solver2/runsat.sh
-chmod +x solvers/solver3/runsat.sh
+# rm -rf inputs/AUTOGEN*
+# mkdir ./fuzzed-tests
+# chmod 777 ./fuzzed-tests/
+# chmod +x solvers/solver1/runsat.sh
+# chmod +x solvers/solver2/runsat.sh
+# chmod +x solvers/solver3/runsat.sh
 
 g++ ./fuzzer/main.cpp -g -std=c++17 -pthread -O3 -o fuzz-sat
 chmod +x fuzz-sat
-ls
 # mkdir inputs
 
 # Implement your build script here...
