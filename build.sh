07e9e87faae250be2f165f108c3416ce20aee40c
stytSofo <ssofok02@ucy.ac.cy>
2024-02-01 20:21:35 +0000

    creating dirs + permissions

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ if [ -d ./fuzzed-tests ]; then
     rm -rf ./fuzzed-tests
 fi
 
-# rm -rf inputs/AUTOGEN*
-# mkdir ./fuzzed-tests
+rm -rf inputs/AUTOGEN*
+mkdir ./fuzzed-tests
+chmod 777 ./fuzzed-tests/
 chmod +x solvers/solver1/runsat.sh
 chmod +x solvers/solver2/runsat.sh
 chmod +x solvers/solver3/runsat.sh
@@ -16,7 +17,7 @@ chmod +x solvers/solver3/runsat.sh
 g++ ./fuzzer/main.cpp -g -std=c++17 -pthread -O3 -o fuzz-sat
 chmod +x fuzz-sat
 ls
-# mkdir inputs
+mkdir inputs
 
 # Implement your build script here...
 # You can assume this runs on an environment similar to a lab-machine.
