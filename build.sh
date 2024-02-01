df71c10492a695c1f8084cf140ee33835c190cd6
Sergio Miguez <sm3823@ic.ac.uk>
2024-02-01 21:54:44 +0000

    testing

--- build.sh
+++ build.sh
@@ -18,6 +18,8 @@ chmod +x solvers/solver3/runsat.sh
 g++ ./fuzzer/main.cpp -g -std=c++17 -pthread -O3 -o fuzz-sat
 # chmod +x fuzz-sat
 touch ./fuzzed-tests/Testing.txt
+echo "Testing" > ./fuzzed-tests/Testing.txt
+cat ./fuzzed-tests/Testing.txt
 # mkdir inputs
 
 # Implement your build script here...
